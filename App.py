8b23658b58bdf02cb333f0d910d18b4bb6b5a1c9
TDR-2002 <109813564+TDR-2002@users.noreply.github.com>
2025-03-05 11:26:43 +0530

    Update App.py

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ import pickle
 import pandas as pd
 
 # Load the trained model
-with open("sentiment_model (1).pkl", "rb") as file:
+with open("sentiment_model.pkl, "rb") as file:
     model = pickle.load(file)
 
 # Streamlit UI
